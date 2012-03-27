65c598e66f390170083b4593b93736dbb9b92670
Jeff Su <me@jeffsu.com>
2012-03-27 12:45:21 -0700

    preparing for npm

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,8 @@
 require('mochiscript');
 
+console.log('Testing data-store');
 require('./data-store');
+console.log('Testing average');
 require('./average');
+
+setTimeout(function () { console.log("Passed!") }, 2000);
