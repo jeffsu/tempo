e29991f734489818ee07d84616f142a6b6bcab22
Jeff Su <me@jeffsu.com>
2012-03-27 22:21:18 -0700

    adding sync

--- tests/index.js
+++ tests/index.js
@@ -2,7 +2,11 @@ require('mochiscript');
 
 console.log('Testing data-store');
 require('./data-store');
+
 console.log('Testing average');
 require('./average');
+
+console.log('Testing sync');
+require('./sync');
 
 setTimeout(function () { console.log("Passed!") }, 2000);
