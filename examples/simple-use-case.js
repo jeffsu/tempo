54f4ba716a2c2dc75fbaef2173817addff7dbf69
Jeff Su <me@jeffsu.com>
2012-06-01 09:31:08 -0700

    cleaning up sparse logger

--- examples/simple-use-case.js
+++ examples/simple-use-case.js
@@ -4,6 +4,8 @@ var tempo   = require('../lib/tempo');
 var min  = tempo.min();
 var hour = tempo.hour();
 
+var logger = new tempo.SparseLogger(10);
+
 var urls = {};
 var ids  = {};
 var id   = 0;
@@ -27,6 +29,7 @@ app.use(function (req, res, next) {
 app.use(function (req, res, next) { 
   var url = req.url;
   urls[url] = true;
+  logger.log(url);
   min.increment('urls', url); 
   hour.increment('urls', url); 
   res.end("Logging:" + url);
@@ -35,7 +38,9 @@ app.use(function (req, res, next) {
 console.log('listening on port 3333 (go to any url)');
 app.listen(3333);
 
+
 setInterval(function () {
+  console.log("Logs:", JSON.stringify(logger.logs, null, 2));
   console.log("TEMPO REPORT (PAST HOUR):");
   for (var url in urls) {
     console.log('  ' + url);
