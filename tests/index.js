2528c110e97c7a4f77a3ba0be1c72a7079d68792
Jeff Su <me@jeffsu.com>
2012-06-15 09:45:15 -0700

    adding access to all keys and attrs

--- tests/index.js
+++ tests/index.js
@@ -12,4 +12,8 @@ require('./average');
 console.log('Testing sync');
 require('./sync');
 
+console.log('Testing keys and attrs');
+require('./keys-and-attrs');
+
+
 setTimeout(function () { console.log("Passed!"); process.exit(0); }, 2000);
