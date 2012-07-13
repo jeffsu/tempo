a9804c8e7691ade88f178674bfe65f752c2be649
Jeff Su <me@jeffsu.com>
2012-07-13 15:21:27 -0700

    adding push-only

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,6 @@
 require('mochiscript');
 
+/*
 console.log('Sparse Logger');
 require('./sparse-logger');
 
@@ -8,12 +9,15 @@ require('./data-store');
 
 console.log('Testing average');
 require('./average');
+*/
 
 console.log('Testing sync');
 require('./sync');
 
+/*
 console.log('Testing keys and attrs');
 require('./keys-and-attrs');
+*/
 
 
 setTimeout(function () { console.log("Passed!"); process.exit(0); }, 2000);
