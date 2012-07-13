4637449247166fd8026b4b0fc6656c77dbcc37fc
Jeff Su <me@jeffsu.com>
2012-07-13 15:23:15 -0700

    fixing tests

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,5 @@
 require('mochiscript');
 
-/*
 console.log('Sparse Logger');
 require('./sparse-logger');
 
@@ -9,15 +8,12 @@ require('./data-store');
 
 console.log('Testing average');
 require('./average');
-*/
 
 console.log('Testing sync');
 require('./sync');
 
-/*
 console.log('Testing keys and attrs');
 require('./keys-and-attrs');
-*/
 
 
-setTimeout(function () { console.log("Passed!"); process.exit(0); }, 2000);
+setTimeout(function () { console.log("Passed!"); process.exit(0); }, 3000);
