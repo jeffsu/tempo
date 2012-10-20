15b40c284022407482dc2b96ade020bb113f0f83
Jeff Su <me@jeffsu.com>
2012-10-19 17:58:01 -0700

    working on faster version of tempo

--- tests/mem-leak.js
+++ tests/mem-leak.js
@@ -1,6 +1,15 @@
 var tempo = require('../lib/tempo');
-var counter = new tempo.TimedCounter({ per: 100, buckets: 20 });
+var counter = new tempo.TimedCounter({ per: 10, buckets: 200 });
 
 setInterval(function () {
   for (var i=0; i<10000; i++) counter.inc(i);
-}, 100);
+  var foo = counter.getHistory(0);
+}, 5);
+
+var prev = process.memoryUsage();
+setInterval(function () {
+  var now = process.memoryUsage();
+  for (var k in now) 
+    console.log(k + ': ' + (now[k] - prev[k]));
+  prev = now;
+}, 10000);
