3770a05c6c23051e73e0b55e05e36c21e838d7f9
agate <agate.hao@gmail.com>
2017-01-03 20:44:36 +0800

    fixed tests

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,12 @@
 require('mochiscript');
-var reporter = require('nodeunit').reporters.default;
-reporter.run([ 'tests/functional.ms', 'tests/increment.ms', 'tests/iterators.ms', 'tests/syncer.ms', 'tests/offsets.ms' ]);
+
+require('nodeunit')
+  .reporters
+  .default
+  .run([
+    'tests/functional.ms',
+    'tests/increment.ms',
+    'tests/iterators.ms',
+    'tests/syncer.ms',
+    'tests/offsets.ms'
+  ]);
